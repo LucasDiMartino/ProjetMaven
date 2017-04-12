249e85687b267546cda8907636c4743c7e335db7
Di Martino Lucas <lucas.di_martino@insa-cvl.fr>
2017-04-12 15:11:45 +0200

    lulu projet en force

--- lulu/src/main/java/lulu/App.java
+++ lulu/src/main/java/lulu/App.java
@@ -1,13 +1,44 @@
-package lulu;
+package example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
+import java.io.*;
+import java.util.*;
+import com.opencsv.*;
+import org.apache.commons.lang.WordUtils;
+
+
+public class Hello {
+
+    public static int max(int a, int b)
     {
-        System.out.println( "Hello World!" );
+    	return a > b ? a : b;
+    }
+
+    public static void main(String[] args) {
+        String  message = "hello ivy !";
+        System.out.println("standard message : " + message);
+        System.out.println("capitalized by " + WordUtils.class.getName() 
+                                + " : " + WordUtils.capitalizeFully(message));
+	
+	try {
+    	CSVReader reader = new CSVReader(new FileReader("data.csv"),',');
+    	List<String[]> myEntries = reader.readAll();
+	int monmax = 0;        
+	for(String[] s : myEntries)
+        {
+		for(String nb : s)
+		{
+			System.out.println("Nombre lu : "+ nb);
+                	monmax = max(monmax, Integer.parseInt(nb));
+
+		}
+        }
+        System.out.println("Mon max : " + monmax);
+	
+	}
+	catch(Exception e)
+	{
+		System.out.println("Erreu" + e.getMessage());
+	}
     }
 }
+        
